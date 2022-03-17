ed66b56341e11e456b72984295690537f40057d5
Quentin D.C <quent043@hotmail.com>
2022-03-17 14:43:53 +0000

    TS Version

---
+++ testAlgo.ts
@@ -0,0 +1,39 @@
+// function “closestToZero” described by the following specifications:
+//
+//     given an array of positive and negative integers, it returns the closest number to zero
+//
+// if the closest number in input could be either negative or positive, the function returns the positive one
+//
+// if the input array is undefined or empty, the function returns 0
+
+function closestToZero(input: number[] | undefined): number {
+    if (!input || input.length === 0) return 0;
+    let result = input[0];
+    const negativeNumbers: number[] = [];
+    for (let item of input) {
+        if (item < 0) negativeNumbers.push(item)
+        else if (item < result) result = item;
+    }
+
+    if (negativeNumbers.length === 0) return result;
+
+    const absoluteValueArray = negativeNumbers.map(Math.abs);
+    absoluteValueArray.forEach(value => {
+        if (value < result) result = -value;
+    })
+    return result;
+}
+
+
+let input = [1, 30, 5, 8, 6, -2, -9]; //1
+let input2 = [4, 30, 5, 8, 6, -2, -9]; // -2
+let input3 = [5, 30, 5, 8, 2, -2, -9]; // 2
+let input4 = [4, 30, 5, 8, 6, 0, -9]; // 0
+let input5: number[] = []; //0
+let input6 = undefined; //0
+console.log(closestToZero(input));
+console.log(closestToZero(input2));
+console.log(closestToZero(input3));
+console.log(closestToZero(input4));
+console.log(closestToZero(input5));
+console.log(closestToZero(input6));
